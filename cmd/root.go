7fb3adcce0191a000f7fcd03a58e294ad9fe8730
Alex Bolduc <axbolduc@gmail.com>
2024-02-18 22:22:18 -0500

    feat: update cmd structure

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,38 @@
 package cmd
 
 import (
+	"gomlb/ui"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+var inputDate string
+
 var rootCmd = &cobra.Command{
 	Use:   "gomlb",
-	Short: "CLI for sports",
-	Long:  "CLI application for multiple sports",
+	Short: "TUI for MLB",
+	Long:  "TUI application for viewing live and past MLB games and statistics",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		var date time.Time
+		var err error
+		if inputDate == "" {
+			date = time.Now()
+		} else {
+			date, err = time.Parse(time.DateOnly, inputDate)
+			if err != nil {
+				panic("Invalid date")
+			}
+		}
+
+		ui.StartTea(date)
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&inputDate, "date", "d", "", "Date to get the schedule for (YYYY-MM-DD)")
 }
 
 func Execute() {
