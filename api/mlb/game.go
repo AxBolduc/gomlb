e3d973b374e04499b66a0c45e6010e0f471535a1
Alex Bolduc <axbolduc@gmail.com>
2024-02-25 16:38:09 -0500

    feat: update game list data

--- api/mlb/game.go
+++ api/mlb/game.go
@@ -2,6 +2,8 @@ package mlb
 
 import (
 	"fmt"
+	"log"
+	"time"
 )
 
 type GameTeams struct {
@@ -12,9 +14,20 @@ type GameTeams struct {
 type Game struct {
 	GamePk    int
 	GameGuid  string
+	GameDate  string
 	Teams     GameTeams
 	Linescore ScheduleLinescore
 	Lineups   ScheduleLineups
+	Status    GameStatus
+}
+
+type GameStatus struct {
+	AbstractGameState string
+	CodedGameState    string
+	DetailedState     string
+	StatusCode        string
+	StartTimeTBD      bool
+	AbstractGameCode  string
 }
 
 func (g Game) FilterValue() string {
@@ -22,11 +35,29 @@ func (g Game) FilterValue() string {
 }
 
 func (g Game) Title() string {
-	return fmt.Sprintf("%s v %s", g.Teams.Away.Team.Name, g.Teams.Home.Team.Name)
+	date, err := time.Parse(time.RFC3339, g.GameDate)
+	if err != nil {
+		log.Printf("Failed to parse game time, defaulting")
+		date = time.Now()
+	}
+
+	dateString := date.Local().Format(time.Kitchen)
+	if g.Status.CodedGameState == "I" {
+		dateString = "LIVE"
+	}
+
+	return fmt.Sprintf("%s (%d-%d) v %s (%d-%d) - (%s)",
+		g.Teams.Away.Team.Name,
+		g.Teams.Away.LeagueRecord.Wins,
+		g.Teams.Away.LeagueRecord.Losses,
+		g.Teams.Home.Team.Name,
+		g.Teams.Home.LeagueRecord.Wins,
+		g.Teams.Home.LeagueRecord.Losses,
+		dateString,
+	)
 }
 
 func (g Game) Description() string {
-	return fmt.Sprintf("(%s) - (%s)",
-		g.Teams.Away.LeagueRecord.Pct,
-		g.Teams.Home.LeagueRecord.Pct)
+	// TODO figure something out to put here
+	return g.Status.DetailedState
 }
